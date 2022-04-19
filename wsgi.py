be3fb5153c20b2ea03b60a3d2ae4e0666a9bfaca
Yatharth <shah9yatharth@gmail.com>
2022-04-19 13:25:42 +0530

    Try 1 25 pm

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-# This is the WSGI Python script that will be called by the web server.
-from AppCode import app
-def main():
-    app.run(host='localhost')
-
-
-if __name__== "__main__":
-    main()
